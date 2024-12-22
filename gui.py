9f3c1c9ecb458d2c56de050c27954eeec34e8532
Samvit Dammalapati <samvit1998@gmail.com>
2024-12-22 09:42:03 +0530

    basic gui

---
+++ gui.py
@@ -0,0 +1,8 @@
+# Note that this is a basic implementation and doesn't include some advanced chess rules like:
+# Check and checkmate detection
+# En passant
+# Castling
+# Pawn promotion
+# Move history
+# Game state saving
+# These features could be added with additional JavaScript code if needed.
